685f8339e3f6ebb675bc18d1c7ca2ab83648c83d
ami-GS <1991.daiki@gmail.com>
2017-06-07 23:42:25 +0900

    change more flexible

--- python/app.py
+++ python/app.py
@@ -2,38 +2,55 @@ from layer import FullyConnect, Conv2D, MaxPooling2D
 from activation import Sigmoid, ReLU, Tanh, Softmax
 from optimizer import Momentum
 from network import Network
-from loss import MSE
+from loss import MSE, CrossEntropy
 import numpy as np
 
 if __name__ == "__main__":
     dataNum = 5000
     learning_rate = 0.001
 
-    # 8*8 image
-    input_shape = 64
+    # 12 * 12 image
+    input_shape = 144
     epoch = 10
-    last_units = 1
-    dataset = np.zeros((dataNum, input_shape))
-    dataset[:dataNum/2,:] = np.random.uniform(-1, 0, (dataNum/2,input_shape))
-    dataset[dataNum/2:,:] = np.random.uniform(0, 1, (dataNum/2,input_shape))
+    batchSize = 50
+    channel = 1
+    last_units = 2
 
+    dataset = np.zeros((dataNum, channel, input_shape)) # 1 for channel
     label = np.zeros((dataNum, last_units))
-    label[:dataNum/2,:] = np.ones((dataNum/2, last_units))
-
+    for i in range(last_units):
+        # currently -1~0. 0~1 can be used due to some issue
+        dataset[dataNum/last_units*i:dataNum/last_units*(i+1),:,:] = np.random.uniform(i-1, i, (dataNum/last_units,channel,input_shape))
+        label[dataNum/last_units*i:dataNum/last_units*(i+1),i] = 1
     net = Network(
-        [Conv2D(4, 1, 3, input_shape=input_shape), # 64 * 4 -> 36*4
+        [Conv2D(4, 1, 3, input_shape=input_shape), # 1*12*12  -> 4*10*10
          ReLU(),
-         MaxPooling2D(2), # 6*6*4 -> 5*5*4
-         FullyConnect(units=last_units, input_shape=100)],
-         #FullyConnect(units=last_units)],
+         MaxPooling2D(2), # 10*10*4 -> 9*9*4
+         FullyConnect(units=last_units, input_shape=324),
+         Softmax(),
+     ],
+    #FullyConnect(units=last_units)],
         # FullyConnect(units=last_units, input_shape=144)],
         learning_rate = learning_rate,
-        optimizer=Momentum(0.02))
+        optimizer=Momentum(0.9),
+        batch=batchSize)
+
+    err_prev = 0
     for e in range(epoch):
-        print e
-        for i in range(dataNum):
-            net.train(dataset[i], label[i], loss=MSE())
-        
+        err = 0
+        for batchIdx in range(0, dataNum, batchSize):
+            batchData = dataset[batchIdx:batchIdx + batchSize,:,:]
+            batchLabel = label[batchIdx:batchIdx + batchSize,:]
+            err += net.train(batchData, batchLabel, loss=MSE())
+        print "epoch", e
+        print "\t", err
+        if abs(err_prev - err) < 10e-15:
+            print "early_stop"
+            break
+        err_prev = err
+
+    print net.predict(dataset[0, :, :].reshape(1,1,input_shape)), label[0,:]
+    print net.predict(dataset[dataNum-1, :].reshape(1,1,input_shape)), label[dataNum-1,:]
 
     """
     # 100 epoch 10 batch  8*8 image
@@ -70,8 +87,7 @@ if __name__ == "__main__":
             print "epoch", e+1
             print "\t", err
     print dataset.shape, label.shape
+    print net.predict(dataset[0, :], label[0,:]
+    print net.predict(dataset[dataNum-1, :], label[dataNum-1,:]
+    #ans = net.predict(dataset[:,:], batchS.ize)
     """
-
-    print net.predict(dataset[0, :]), label[0,:]
-    print net.predict(dataset[dataNum-1, :]), label[dataNum-1,:]
-    #ans = net.predict(dataset[:,:], batchSize)
