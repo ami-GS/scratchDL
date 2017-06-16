8a10e7b5e7267bc8c8ccba36fc4d762e7e06ad3a
ami-GS <1991.daiki@gmail.com>
2017-06-17 02:26:57 +0900

    optimizatoin of convolutoin. could be better stil

--- python/layer.py
+++ python/layer.py
@@ -26,7 +26,7 @@ class Conv2D(Layer):
         super(Conv2D, self).__init__(input_shape, None)
         self.filterNum = filters
         self.kernel_size = kernel_size
-        self.filters = np.random.uniform(-1, 1, (filters, kernel_size, kernel_size))
+        self.filters = np.random.uniform(-1, 1, (filters, kernel_size**2))
         self.strides = strides
         if input_shape:
             # size is valid
@@ -45,32 +45,37 @@ class Conv2D(Layer):
             self.input_shape = prevLayer.units
         self.x_rowcol = int(np.sqrt(self.input_shape))
         self.y_rowcol = self.x_rowcol - self.kernel_size+1
-        self.X = np.zeros((self.batch, self.channel, self.x_rowcol, self.x_rowcol))
-        self.E = np.zeros((self.batch, self.filterNum, self.y_rowcol, self.y_rowcol))
+        self.X = np.zeros((self.batch, self.channel, self.kernel_size**2, self.y_rowcol**2))
+        self.E = np.zeros((self.batch, self.filterNum, self.y_rowcol**2))
         self.err_delta = np.zeros((self.batch, self.channel, self.x_rowcol, self.x_rowcol))
-        self.Y = np.zeros((self.batch, self.filterNum, self.y_rowcol, self.y_rowcol))
+        self.Y = np.zeros((self.batch, self.filterNum, self.y_rowcol**2))
 
     def forward(self, x):
-        self.X[:] = np.reshape(x, (self.batch, self.channel, self.x_rowcol, self.x_rowcol))
-        for yi in range(0, self.y_rowcol, self.strides[0]):
-            for yj in range(0, self.y_rowcol, self.strides[1]):
-                self.Y[:,:,yi,yj] = np.sum(np.einsum("bcij,fij->bcfij", self.X[:,:,yi:yi+self.kernel_size,yj:yj+self.kernel_size], self.filters[:,:,:]), axis=(1,3,4))
-        return self.Y
+        x = x.reshape(self.batch, self.channel, self.x_rowcol, self.x_rowcol)
+        for i in range(self.y_rowcol):
+            for j in range(self.y_rowcol):
+                self.X[:, :, :, self.y_rowcol*i+j%self.y_rowcol] = \
+                x[:, :, i:i+self.kernel_size, j:j+self.kernel_size].reshape(self.batch, self.channel, self.kernel_size**2)
+
+        for b in range(self.batch):
+            for c in range(self.channel):
+                self.Y[b, :] += self.filters.dot(self.X[b,c])
+        return self.Y.reshape(self.batch, self.filterNum, self.y_rowcol, self.y_rowcol)
 
     def backward(self, err_delta):
-        self.E[:] = err_delta
-        template_shape = (self.batch, self.channel, self.kernel_size, self.kernel_size)
+        self.E[:] = err_delta.reshape(self.batch, self.filterNum, self.y_rowcol**2)
+
         for yi in range(self.y_rowcol):
             for yj in range(self.y_rowcol):
-                self.err_delta[:, :, yi:yi+self.kernel_size, yj:yj+self.kernel_size] = \
-                        np.reshape(np.squeeze(self.err_delta[:, :, yi:yi+self.kernel_size, yj:yj+self.kernel_size]) + \
-                        np.sum(np.einsum("bf,fij->bfij", self.E[:,:,yi,yj], self.filters), axis=1), template_shape)
+                self.err_delta[:, :, yi:yi+self.kernel_size, yj:yj+self.kernel_size] += \
+                        np.sum(np.einsum("bf,fk->bfk", self.E[:,:,yi*self.y_rowcol + yj%self.y_rowcol], self.filters), axis=1)\
+                          .reshape(self.batch, self.channel, self.kernel_size, self.kernel_size)
 
-        for yi in range(0, self.y_rowcol, self.strides[0]):
-            for yj in range(0, self.y_rowcol, self.strides[1]):
-                self.filters -= np.sum(np.sum(
-                    np.einsum("bcij,bf->bcfij", self.X[:,:,yi:yi+self.kernel_size,yj:yj+self.kernel_size], self.E[:,:,yi,yj]),
-                    axis=1), axis=0)/self.batch #sum channel, then batch
+        tmp = np.zeros(self.filters.shape)
+        for b in range(self.batch):
+            for c in range(self.channel):
+                tmp += self.X[b,c,:,:].dot(self.E[b,:,:].T).T
+        self.filters -= tmp/self.batch
 
         return self.err_delta
 
