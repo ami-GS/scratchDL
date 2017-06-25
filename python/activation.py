bf27fecc04ce956b6ae52a4fe711bf2f20621087
ami-GS <1991.daiki@gmail.com>
2017-06-25 11:15:22 +0900

    use phase for saving resource

--- python/activation.py
+++ python/activation.py
@@ -10,10 +10,12 @@ class Sigmoid(Activation):
         super(Sigmoid, self).__init__()
 
     def forward(self, x):
-        # destructive assignment
-        self.Y = np.reciprocal(1 + np.exp(-x))
-
-        return self.Y
+        if self.phase == "TRAIN":
+            # destructive assignment
+            self.Y = np.reciprocal(1 + np.exp(-x))
+            return self.Y
+        else:
+            return np.reciprocal(1 + np.exp(-x))
 
     def backward(self, err_delta):
         return np.multiply(err_delta, np.multiply(self.Y, 1-self.Y))
@@ -23,9 +25,12 @@ class ReLU(Activation):
         super(ReLU, self).__init__()
 
     def forward(self, x):
-        # destructive assignment
-        self.Y = np.multiply(np.greater_equal(x, 0), x)
-        return self.Y
+        if self.phase == "TRAIN":
+            # destructive assignment
+            self.Y = np.multiply(np.greater_equal(x, 0), x)
+            return self.Y
+        else:
+            return np.multiply(np.greater_equal(x, 0), x)
 
     def backward(self, err_delta):
         return err_delta * np.greater(self.Y, 0)*1
@@ -35,10 +40,12 @@ class Tanh(Activation):
         super(Tanh, self).__init__()
 
     def forward(self, x):
-        # destructive assignment
-        self.Y = np.tanh(x)
-
-        return self.Y
+        if self.phase == "TRAIN":
+            # destructive assignment
+            self.Y = np.tanh(x)
+            return self.Y
+        else:
+            return np.tanh(x)
 
     def backward(self, err_delta):
         return np.multiply(err_delta, 1 - np.power(self.Y, 2))
