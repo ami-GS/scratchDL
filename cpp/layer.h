a4eb980ea97c51487c4f7f7e2d57717e90ead421
ami-GS <1991.daiki@gmail.com>
2017-06-28 21:55:12 +0900

    add minimum methods

--- cpp/layer.h
+++ cpp/layer.h
@@ -5,15 +5,18 @@ class Layer {
 public:
     Layer(int input_shape, int units);
     virtual ~Layer();
-    virtual configure();
-    virtual forward();
-    virtual backward();
+    virtual int configure(int batch, Layer* prevLayer) = 0;
+    virtual void forward(float* x) = 0;
+    virtual void backward(float* e) = 0;
 };
 
 class FullyConnect : public Layer {
 public:
     FullyConnect(int input_shape, int units);
     ~FullyConnect();
+    int configure(int batch, Layer* prevLayer);
+    void forward(float* x);
+    void backward(float* e);
 };
 
 #endif // CPP_LAYER_H_
