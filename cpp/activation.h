9e76a3e10d0327ce96e27e4e6c3954a39d84ebbb
ami-GS <1991.daiki@gmail.com>
2017-06-30 22:11:34 +0900

    add activation class

---
+++ cpp/activation.h
@@ -0,0 +1,25 @@
+#ifndef CPP_ACTIVATION_H_
+#define CPP_ACTIVATION_H_
+
+#include "layer.h"
+#include <stdlib.h>
+
+class Activation : public Layer {
+public:
+    Activation();
+    ~Activation();
+    int configure(int batch, Layer* prevLayer);
+    virtual void forward(float* x) = 0;
+    virtual void backward(float* e) = 0;
+};
+
+class Sigmoid : public Activation {
+public:
+    Sigmoid();
+    ~Sigmoid();
+    void forward(float* x);
+    void backward(float* e);
+};
+
+
+#endif // CPP_ACTIVATION_H_
