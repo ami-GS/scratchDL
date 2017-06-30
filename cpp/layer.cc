43b4f43b5c2a30eadeab7f68521ea506e715b413
ami-GS <1991.daiki@gmail.com>
2017-06-30 22:11:34 +0900

    implement backward of fc

--- cpp/layer.cc
+++ cpp/layer.cc
@@ -53,5 +53,22 @@ void FullyConnect::forward(float* x) {
 }
 
 void FullyConnect::backward(float* e) {
+    for (int b = 0; b < this->batch; b++) {
+        for (int u = 0; u < this->units; u++) {
+            for (int i = 0; i < this->input_shape; i++) {
+                this->E[b*this->input_shape + i] += e[b*this->units + u] * this->W[i*this->units+u];
+            }
+        }
+    }
+
+    for (int b = 0; b < this->batch; b++) {
+        for (int i = 0; i < this->input_shape; i++) {
+            for (int u = 0; u < this->units; u++) {
+                // TODO : not good for division in each calc
+                this->W[i*this->units + u] -= (this->X[b*this->input_shape + i] * e[b*this->units + u])/this->batch;
+            }
+        }
+    }
+
     return;
 }
