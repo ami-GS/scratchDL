a3550fe0fa85295e58f25a86edb4d8645324409f
ami-GS <1991.daiki@gmail.com>
2017-07-05 23:50:40 +0900

    implement loss

---
+++ cpp/loss.cc
@@ -0,0 +1,35 @@
+#include "loss.h"
+#include <cmath>
+#include <stdlib.h>
+
+Loss::Loss() {}
+Loss::~Loss() {
+    delete this->D;
+}
+
+MSE::MSE() {}
+MSE::~MSE() {}
+
+int MSE::configure(int batch, Layer* prevLayer) {
+    if (prevLayer != nullptr) {
+        this->prevLayer = prevLayer;
+    }
+    this->batch = batch;
+    this->D = (float*)malloc(sizeof(float)*this->batch*this->prevLayer->units);
+    return 1;
+}
+
+float MSE::error(float* x, int* label) {
+    float e = 0;
+    for (int i = 0; i < this->prevLayer->units; i++) {
+        e += std::pow(std::abs(x[i]-(float)label[i]), 2)*0.5;
+    }
+    return e;
+}
+
+void MSE::partial_derivative(float* x, int* label) {
+    for (int i = 0; i < this->batch*this->prevLayer->units; i++) {
+        this->D[i] = - ((float)label[i] - x[i]);
+    }
+    return;
+}
---
+++ cpp/loss.h
@@ -0,0 +1,28 @@
+#ifndef CPP_LOSS_H_
+#define CPP_LOSS_H_
+
+#include "layer.h"
+
+
+class Loss {
+public:
+    float* D;
+    int batch;
+    Layer* prevLayer;
+    Loss();
+    ~Loss();
+    virtual int configure(int batch, Layer* prevLayer) = 0;
+    virtual float error(float* x, int* label) = 0;
+    virtual void partial_derivative(float* x, int* label) = 0;
+};
+
+class MSE : public Loss {
+public:
+    MSE();
+    ~MSE();
+    int configure(int batch, Layer* prevLayer);
+    float error(float* x, int* label);
+    void partial_derivative(float* x, int* label);
+};
+
+#endif // CPP_LOSS_H_
