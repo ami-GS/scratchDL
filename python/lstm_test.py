a3501b1a69e2e6928c3ff48b4bc83ebfd81f6069
ami-GS <1991.daiki@gmail.com>
2017-08-13 23:47:03 +0900

    apply output layer

--- python/lstm_test.py
+++ python/lstm_test.py
@@ -8,19 +8,22 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     dataNum = 100
-    dataPoints = 10**3
-    learning_rate = 0.001
+    dataPoints = 2**14
+    learning_rate = 0.01
     # X shold be varried
     dataX = np.zeros((dataNum, dataPoints))
     for i in range(dataNum):
         prefix = np.random.rand()*2
         dataX[i,:] = np.linspace(prefix, prefix+np.pi*4, dataPoints)
     labelY = np.sin(dataX)
-    epoch = 8000
-    batchSize = 10
-    input_shape = 100
-    units = 20
-    lstm = Network([LSTM(units, input_shape)],
+    epoch = 60
+    batchSize = 20
+    input_shape = 2048
+    units = 256
+    last_units = 64
+    lstm = Network([LSTM(units, input_shape),
+                    FullyConnect(units=last_units, input_shape=units)
+                ],
                    learning_rate=learning_rate,
                    optimizer=Momentum(0.9),
                    batch=batchSize,
@@ -32,12 +35,12 @@ if __name__ == "__main__":
         for batchIdx in range(0, dataNum, batchSize):
             batchData = labelY[batchIdx:batchIdx + batchSize, :]
             for timeIdx in range(0, dataPoints-input_shape, input_shape):
-                err += lstm.train(batchData[:, timeIdx:timeIdx+input_shape], batchData[:, timeIdx+input_shape:timeIdx+input_shape+units], loss=MSE())
+                err += lstm.train(batchData[:, timeIdx:timeIdx+input_shape], batchData[:, timeIdx+input_shape:timeIdx+input_shape+last_units], loss=MSE())
         print "epoch", e
         print "\t", err/batchSize
         err_prev = err
 
     print lstm.predict(labelY[:batchSize, :input_shape]), labelY[:batchSize, input_shape:input_shape+units]
     nxt = lstm.predict(labelY[:batchSize,:input_shape])
-    plt.plot(dataX[0,input_shape:input_shape+units], nxt[0, :], dataX[0, input_shape:input_shape+units], labelY[0, input_shape:input_shape+units])
+    plt.plot(dataX[0,input_shape:input_shape+last_units], nxt[0, :], dataX[0, input_shape:input_shape+last_units], labelY[0, input_shape:input_shape+last_units])
     plt.show()
